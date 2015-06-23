cc7562697a9dc0a0ab9e463a4ad63dd9d05c6ca4
Paolo Cifariello <paolocifa@gmail.com>
2015-06-23 12:29:04 +0200

    refactoring namespaces

---
+++ test/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using PA_Final.Scanning;
+using PA_Final.Parsing;
+using PA_Final.Utils;
+
+
+namespace PA_Final.Test
+{
+	public static class Test1
+	{
+		public static void Test ()
+		{
+			string[] tests = {
+				"../../test/res/test1",
+				"../../test/res/test2"
+			};
+
+			foreach (string test in tests) {
+
+				var input = File.ReadAllText (test);
+				var parser = new Parser (input);
+
+				var graph = parser.Parse ();
+
+
+			}
+
+			Console.ReadLine ();
+		}
+	}
+}
